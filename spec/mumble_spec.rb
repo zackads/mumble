1c6d4a2342e9bc16f7be0bb487099e6725be6d31
Claire Guest <claire.guest@madetech.com>
2020-09-25 15:55:32 +0100

    Fails two upper case letters test

--- spec/mumble_spec.rb
+++ spec/mumble_spec.rb
@@ -19,5 +19,11 @@ describe Mumble do
         expect(Mumble.new.mumble_letters('B')).to eq 'B'
       end
     end
+
+    context 'given a string of two upper case characters' do
+      it 'returns the string mumbled' do
+        expect(Mumble.new.mumble_letters('AB')).to eq 'A-Bb'
+      end
+    end
   end
 end
