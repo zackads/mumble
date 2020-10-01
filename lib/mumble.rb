940f1d77165a2d90716d9b45b84fd909753ac077
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-10-01 13:41:08 +0100

    Constant -> scalar for input values 'AB', 'BC', 'CD' etc.

--- lib/mumble.rb
+++ lib/mumble.rb
@@ -2,8 +2,6 @@ class Mumble
   def mumble_letters(string)
     return string if string.length <= 1
 
-    return 'A-Bb' if string == 'AB'
-
-    return 'B-Cc'
+    string[0] + '-' + string[1] + string[1].downcase
   end
 end
